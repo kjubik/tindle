e429578464c75cd85960f29f18f0c4323cc03771
kjubik <wojt.kubicki@gmail.com>
2023-06-11 23:47:34 +0200

    base skeleton of landing page

---
+++ src/components/GetJsonData.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { onMount } from "svelte";
+    
+    export let link = "";
+
+    let list = []
+
+    onMount(async () => {
+        const response = await fetch(link)
+        list = await response.json()
+        console.log(list)
+    })
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    
+    
+
+</script>
+
+<main>
+    <div class="container">
+        <div id="header">
+            <h1>tindle</h1>
+            <h2>interfejs wyników z Technologii internetowych</h2>
+        </div>
+
+        <div id="actions">
+            <div class="search-bar">
+                <input type="text" placeholder="Wpisz indeks">
+                <button>Szukaj</button>
+            </div>
+            <p class="small-text">lub</p>
+            <div class="button">
+                <button>Zobacz wyniki wszystkich</button>
+            </div>
+        </div>
+    </div>
+</main>
+
+<style>
+
+</style>
