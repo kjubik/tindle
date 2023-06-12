060759898c0709f428cb345f95b616bae0e752ec
kjubik <wojt.kubicki@gmail.com>
2023-06-13 00:57:17 +0200

    niewiarygodny syf w sortowaniu tabel

--- src/routes/results/all/+page.svelte
+++ src/routes/results/all/+page.svelte
@@ -2,8 +2,12 @@
     import { onMount } from "svelte";
 
     let link = 'http://bikol.vm.wmi.amu.edu.pl/dtin/results'
-    let object = {}
+    
     let results = []
+    let jsonArray = []
+    let mappedTasks = []
+    let mappedTasksCopy = []
+    let thisIsGettingOutOfHand = []
 
     onMount(async () => {
         try {
@@ -11,9 +15,40 @@
 
             if (response.ok) {
                 results = await response.json()
-                console.log(results)
-                // var obj = {"1":5,"2":7,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0,"10":0,"11":0,"12":0}
-                // var result = Object.keys(obj).map((key) => [key, obj[key]]);
+
+                // Convert the JSON object to an array of objects
+                jsonArray = Object.entries(results).map(([id, data]) => ({ id, ...data }));
+
+                mappedTasks = jsonArray.map((obj) => {
+                    const { tasks, ...rest } = obj;
+                    const mappedTasks = [];
+
+                    for (const taskKey in tasks) {
+                        if (tasks.hasOwnProperty(taskKey)) {
+                        const task = { task_id: taskKey, ...tasks[taskKey] };
+                        mappedTasks.push(task);
+                        }
+                    }
+
+                    return { ...rest, tasks: mappedTasks };
+                });
+
+                mappedTasksCopy = mappedTasks
+                console.log(mappedTasksCopy)
+                mappedTasksCopy.forEach(item => {
+                    item.tasks.forEach(task => {
+                        task.id = item.id
+                    })
+                })
+
+                thisIsGettingOutOfHand = mappedTasksCopy.flatMap(item => item.tasks);
+                console.log(thisIsGettingOutOfHand)
+                
+                // data.forEach(item => {
+                //     item.tasks.forEach(task => {
+                //         task.id = item.id;
+                //     });
+                // });
             }
         } catch (error) {
             console.log(error)
@@ -26,7 +61,7 @@
     // Holds table sort state.  Initialized to reflect table sorted by id column ascending.
 	let sortBy = {col: "id", ascending: true};
 	
-	$: sort = (column) => {
+	$: sort1 = (column) => {
 		
 		if (sortBy.col == column) {
 			sortBy.ascending = !sortBy.ascending
@@ -45,7 +80,29 @@
 			? 1 * sortModifier 
 			: 0;
 		
-		array = array.sort(sort);
+		mappedTasks = mappedTasks.sort(sort);
+	}
+
+    $: sort2 = (column) => {
+		
+		if (sortBy.col == column) {
+			sortBy.ascending = !sortBy.ascending
+		} else {
+			sortBy.col = column
+			sortBy.ascending = true
+		}
+		
+		// Modifier to sorting function for ascending or descending
+		let sortModifier = (sortBy.ascending) ? 1 : -1;
+		
+		let sort = (a, b) => 
+			(a[column] < b[column]) 
+			? -1 * sortModifier 
+			: (a[column] > b[column]) 
+			? 1 * sortModifier 
+			: 0;
+		
+		thisIsGettingOutOfHand = thisIsGettingOutOfHand.sort(sort);
 	}
 
 </script>
@@ -56,15 +113,15 @@
     <table style="float: left;">
         <thead>
             <tr>
-            <th on:click={ null }>Indeks</th>
-            <th>Suma</th>
+            <th on:click={sort1("id")}>Indeks</th>
+            <th on:click={sort1("score")}>Suma</th>
             </tr>
         </thead>
         <tbody>
-            {#each Object.entries(results) as [indeks, { score }]}
+            {#each mappedTasks as obj}
                 <tr>
-                <td><a href="/results/{indeks}">{indeks}</a></td>
-                <td>{score}</td>
+                <td ><a href="/results/{obj.id}">{obj.id}</a></td>
+                <td>{obj.score}</td>
                 </tr>
             {/each}
         </tbody>
@@ -73,20 +130,18 @@
     <table style="float: left; margin-left: 64px">
         <thead>
             <tr>
-            <th>Indeks</th>
-            <th>Task Number</th>
-            <th>Score</th>
+            <th on:click={ sort2("id") }>Indeks</th>
+            <th on:click={ sort2("task_id") }>Task Number</th>
+            <th on:click={ sort2("score") }>Score</th>
             </tr>
         </thead>
         <tbody>
-            {#each Object.entries(results) as [indeks, { tasks }]}
-            {#each Object.entries(tasks) as [taskNumber, { score }]}
-                <tr>
-                <td><a href="/results/{indeks}">{indeks}</a></td>
-                <td>{taskNumber}</td>
-                <td>{score}</td>
-                </tr>
-            {/each}
+            {#each thisIsGettingOutOfHand as task_result}
+            <tr>
+                <td><a href="/results/{task_result.id}">{task_result.id}</a></td>
+                <td>{task_result.task_id}</td>
+                <td>{task_result.score}</td>
+            </tr>
             {/each}
         </tbody>
     </table>
