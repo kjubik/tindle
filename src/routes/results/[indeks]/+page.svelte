0b2aea55d3fde04aec54aa2528592e57b1790092
Wojciech <wojt.kubicki@gmail.com>
2023-06-12 15:47:09 +0200

    worked on displaying payload data in table

--- src/routes/results/[indeks]/+page.svelte
+++ src/routes/results/[indeks]/+page.svelte
@@ -3,24 +3,18 @@
 
     export let data
     export let user_link = `http://bikol.vm.wmi.amu.edu.pl/dtin/results/${ data.data.indeks }`
-    let base_link = "http://bikol.vm.wmi.amu.edu.pl/dtin/tasks"
-
     let user_results = []
     let taskKeys = []
     let tasks = []
-
-    let base_results = []
-    let baseKeys = []
-    let baseTasks = []
+    
+    let info = {"D1":{"Deadline":"2023-06-09T16:48:52.561805174+02:00","Score":5},"D2":{"Deadline":"2023-06-09T16:48:52.561805414+02:00","Score":5},"D3":{"Deadline":"2023-06-09T16:48:52.561809834+02:00","Score":5},"D4":{"Deadline":"2023-06-09T16:48:52.561812264+02:00","Score":5},"D5":{"Deadline":"2023-06-09T16:48:52.561813624+02:00","Score":5},"D6":{"Deadline":"2023-06-09T16:48:52.561813784+02:00","Score":5},"Z2.1":{"Deadline":"2023-03-30T03:00:00+02:00","Score":2},"Z2.2":{"Deadline":"2023-03-30T03:00:00+02:00","Score":3},"Z2.3":{"Deadline":"2023-03-30T03:00:00+02:00","Score":2},"Z2.4":{"Deadline":"2023-03-30T03:00:00+02:00","Score":2},"Z2.5":{"Deadline":"2023-03-30T03:00:00+02:00","Score":3},"Z3.1":{"Deadline":"2023-04-19T03:00:00+02:00","Score":3},"Z3.2":{"Deadline":"2023-04-19T03:00:00+02:00","Score":4},"Z3.3":{"Deadline":"2023-04-19T03:00:00+02:00","Score":5},"Z5.1":{"Deadline":"2023-06-05T03:00:00+02:00","Score":4},"Z5.2":{"Deadline":"2023-06-05T03:00:00+02:00","Score":2},"Z5.3":{"Deadline":"2023-06-05T03:00:00+02:00","Score":3},"Z5.4":{"Deadline":"2023-06-05T03:00:00+02:00","Score":3},"Z6.1":{"Deadline":"2023-06-15T03:00:00+02:00","Score":4},"Z6.2":{"Deadline":"2023-06-15T03:00:00+02:00","Score":4},"Z6.3":{"Deadline":"2023-06-15T03:00:00+02:00","Score":4},"Z7.1":{"Deadline":"2023-06-26T03:00:00+02:00","Score":7}}
 
     onMount(async () => {
-        // get user results
         try {
             const response = await fetch(user_link)
 
             if (response.ok) {
                 user_results = await response.json()
-                // console.log(user_results)
                 taskKeys = Object.keys(user_results.tasks);
                 tasks = taskKeys.map(key => user_results.tasks[key]);
                 console.log(taskKeys, tasks)
@@ -29,6 +23,25 @@
             console.log(error)
         }
     })
+
+    function matchPayloads(payload1, payload2) {
+        const matchedPayload = {};
+
+        // Matching Z2.x tasks
+        for (const task in payload1.tasks) {
+            if (payload2.hasOwnProperty(task)) {
+            matchedPayload[task] = {
+                time: payload1.tasks[task].time,
+                score1: payload1.tasks[task].score,
+                info1: payload1.tasks[task].info,
+                deadline: payload2[task].Deadline,
+                score2: payload2[task].Score
+            };
+            }
+        }
+
+        return matchedPayload;
+        }
 
     // search bar logic
 
