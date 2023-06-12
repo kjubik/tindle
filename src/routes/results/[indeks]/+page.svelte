706d14adb28073a4bd0d31e27764c413ab85f903
kjubik <wojt.kubicki@gmail.com>
2023-06-12 19:24:27 +0200

    all data from results/[id] and /tasks shown in table

--- src/routes/results/[indeks]/+page.svelte
+++ src/routes/results/[indeks]/+page.svelte
@@ -4,32 +4,53 @@
     export let data
     export let user_link = `http://bikol.vm.wmi.amu.edu.pl/dtin/results/${ data.data.indeks }`
     let user_results = []
-    let taskKeys = []
     let tasks = []
-    
+        
     let info = {"D1":{"Deadline":"2023-06-09T16:48:52.561805174+02:00","Score":5},"D2":{"Deadline":"2023-06-09T16:48:52.561805414+02:00","Score":5},"D3":{"Deadline":"2023-06-09T16:48:52.561809834+02:00","Score":5},"D4":{"Deadline":"2023-06-09T16:48:52.561812264+02:00","Score":5},"D5":{"Deadline":"2023-06-09T16:48:52.561813624+02:00","Score":5},"D6":{"Deadline":"2023-06-09T16:48:52.561813784+02:00","Score":5},"Z2.1":{"Deadline":"2023-03-30T03:00:00+02:00","Score":2},"Z2.2":{"Deadline":"2023-03-30T03:00:00+02:00","Score":3},"Z2.3":{"Deadline":"2023-03-30T03:00:00+02:00","Score":2},"Z2.4":{"Deadline":"2023-03-30T03:00:00+02:00","Score":2},"Z2.5":{"Deadline":"2023-03-30T03:00:00+02:00","Score":3},"Z3.1":{"Deadline":"2023-04-19T03:00:00+02:00","Score":3},"Z3.2":{"Deadline":"2023-04-19T03:00:00+02:00","Score":4},"Z3.3":{"Deadline":"2023-04-19T03:00:00+02:00","Score":5},"Z5.1":{"Deadline":"2023-06-05T03:00:00+02:00","Score":4},"Z5.2":{"Deadline":"2023-06-05T03:00:00+02:00","Score":2},"Z5.3":{"Deadline":"2023-06-05T03:00:00+02:00","Score":3},"Z5.4":{"Deadline":"2023-06-05T03:00:00+02:00","Score":3},"Z6.1":{"Deadline":"2023-06-15T03:00:00+02:00","Score":4},"Z6.2":{"Deadline":"2023-06-15T03:00:00+02:00","Score":4},"Z6.3":{"Deadline":"2023-06-15T03:00:00+02:00","Score":4},"Z7.1":{"Deadline":"2023-06-26T03:00:00+02:00","Score":7}}
 
     onMount(async () => {
         try {
-            const response = await fetch(user_link)
+            const response = await fetch(user_link);
 
             if (response.ok) {
-                user_results = await response.json()
-                // console.log("user results", user_results)
+                user_results = await response.json();
                 tasks = Object.keys(user_results.tasks).map(key => {
                     const task = user_results.tasks[key];
                     task.key = key; // Add the 'key' field to the task object
+
+                    // Check if 'info' object has a property with the same key
+                    if (info.hasOwnProperty(key)) {
+                        const infoData = info[key];
+                        task.Deadline = infoData.Deadline;
+                        task.maxScore = infoData.Score;
+                    }
+
                     return task;
                 });
-                console.log("tasks", tasks)
+
+                // Include objects from 'info' that are not already in 'tasks'
+                Object.keys(info).forEach(key => {
+                    if (!user_results.tasks.hasOwnProperty(key)) {
+                        const task = {
+                            key: key,
+                            Deadline: info[key].Deadline,
+                            maxScore: info[key].Score,
+                            score: 0,
+                            time: null,
+                            info: ''
+                        };
+                        tasks.push(task);
+                    }
+                });
+
+                console.log("tasks", tasks);
             }
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    })
+    });
 
-
-
+    
     // search bar logic
 
     let searchId = ''
@@ -68,14 +89,40 @@
                         <th>Numer</th>
                         <th>Wynik</th>
                         <th>Czas</th>
+                        <th>Termin</th>
+                        <th>Info</th>
                     </tr>
                     </thead>
                     <tbody>
                         {#each tasks as task}
                         <tr>
                                 <td>{task.key}</td>
-                                <td>{task.score}</td>
-                                <td>{(new Date(task.time)).toLocaleString('pl', { dateStyle: 'long', timeStyle: 'short' })}</td>
+                                <td>{task.score} / {task.maxScore}</td>
+                                <td>
+                                    {#if task.time == null}
+                                        brak
+                                    {:else}
+                                        {(new Date(task.time)).toLocaleString('pl', { dateStyle: 'medium', timeStyle: 'short' })}
+                                    {/if}
+                                </td>
+                                <td>{(new Date(task.Deadline)).toLocaleString('pl', { dateStyle: 'medium', timeStyle: 'short' })}</td>
+                                <td>
+                                    {#if task.time != null }
+                                        {#if task.time <= task.Deadline}
+                                            zaliczone
+                                        {:else}
+                                            spóźnione
+                                        {/if}
+                                    {:else}
+                                        {#if new Date(task.Deadline) >= new Date()}
+                                            do zrobienia
+                                        {:else}
+                                            niezaliczone
+                                        {/if}
+                                    {/if}
+                                    
+                                </td>
+                                <td>{task.info}</td>
                         </tr>
                         {/each}
                     </tbody>
