221067ddc81946ab50c66d50c914c2cc2f09e5a3
Wojciech <wojt.kubicki@gmail.com>
2023-06-12 13:12:18 +0200

    displaying data from results/[id] in table

--- src/routes/results/[indeks]/+page.svelte
+++ src/routes/results/[indeks]/+page.svelte
@@ -2,27 +2,57 @@
     import { onMount } from "svelte";
 
     export let data
-    
-    export let link = `http://bikol.vm.wmi.amu.edu.pl/dtin/results/${ data.data.indeks }`
+    export let user_link = `http://bikol.vm.wmi.amu.edu.pl/dtin/results/${ data.data.indeks }`
+    let base_link = "http://bikol.vm.wmi.amu.edu.pl/dtin/tasks"
 
-    let list = []
+    let user_results = []
+    let taskKeys = []
+    let tasks = []
+
+    let base_results = []
+    let baseKeys = []
+    let baseTasks = []
 
     onMount(async () => {
-        const response = await fetch(link)
-        list = await response.json()
-        console.log(list)
+        // get user results
+        try {
+            const response = await fetch(user_link)
+
+            if (response.ok) {
+                user_results = await response.json()
+                // console.log(user_results)
+                taskKeys = Object.keys(user_results.tasks);
+                tasks = taskKeys.map(key => user_results.tasks[key]);
+                console.log(taskKeys, tasks)
+            }
+        } catch (error) {
+            console.log(error)
+        }
     })
+
+    // search bar logic
+
+    let searchId = ''
+
+    function search() {
+        window.location.href = `/results/${searchId}`
+    }
+
+    function handleKeyPress(event) {
+        if (event.key === 'Enter') search()
+    }
+
 </script>
 
 <main>
     <div class="navbar">
         <h1><a href="/">tindle</a></h1>
-        <input type="text" placeholder="Wpisz indeks">
-        <button>Search</button>
+        <input type="text" bind:value={searchId} on:keypress={ handleKeyPress } placeholder="Wpisz indeks">
+        <button on:click={search}>Search</button>
     <div class="content">
         <div class="left-column">
             <div class="points">
-                <h2>{list.score} punkty</h2>
+                <h2>{user_results.score} punkty</h2>
             </div>
             <div class="indeks">
                 <p>indeks <span>{ data.data.indeks }</span></p>
@@ -30,7 +60,26 @@
         </div>
         <div class="right-column">
             <div class="tasks">
-                <h2>Wszystkie wyniki</h2>
+                <h2>Wyniki zadań</h2>
+                
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Numer</th>
+                        <th>Wynik</th>
+                        <th>Czas</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {#each tasks as task, i}
+                        <tr>
+                                <td>{taskKeys[i]}</td>
+                                <td>{task.score}</td>
+                                <td>{(new Date(task.time)).toLocaleString('pl', { dateStyle: 'long', timeStyle: 'short' })}</td>
+                        </tr>
+                        {/each}
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
